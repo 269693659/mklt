391b25ba6f3947531a80e2228eb4edb34c22a683
269693659 <269693659@qq.com>
2020-10-27 17:53:26 +0800

    xx

---
+++ src/views/news/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+      news
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
